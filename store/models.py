3415faa21b41f6b68b45bd80675e7687196c84e0
Nikit Soni <nikitsoni17@gmail.com>
2025-02-16 14:45:11 +0530

    connected mysql and made minor changes to store model

--- store/models.py
+++ store/models.py
@@ -11,7 +11,8 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    slug = models.SlugField(default='-')
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
